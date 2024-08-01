75398bbd4b183aa440a8cc424a33b7c7d7a26649
RaulCoder <adan.morales@udgvirtual.udg.mx>
2024-08-01 16:13:46 -0600

    add GameDataSerializable

--- Assets/_Scripts/Data/GameDataSerializable.cs
+++ Assets/_Scripts/Data/GameDataSerializable.cs
@@ -1,11 +1,23 @@
 ﻿using System;
-using UnityEngine;
+using System.Collections.Generic;
 
-namespace Assets._Scripts
+namespace _Scripts
 {
     [Serializable]
     public class GameDataSerializable
     {
-
+        public List<int> highScoreData;
+        public List<int> currentScoreData;
+        public int finalScore;
+        public float speed;
+        public string difficulty;
+        public GameDataSerializable(GameData gameData)
+        {
+            highScoreData = new List<int>(gameData.HighScoreData);
+            currentScoreData = new List<int>(gameData.CurrentScoreData);
+            finalScore = gameData.FinalScore;
+            speed = gameData.Speed;
+            difficulty = gameData.Difficulty;
+        }
     }
 }
