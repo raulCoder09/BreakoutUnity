aa7a29a9d58a957a06e2bddef73434d60e92ca5c
RaulCoder <adan.morales@udgvirtual.udg.mx>
2024-08-01 16:28:11 -0600

    DataManager

--- Assets/_Scripts/Data/DataManager.cs
+++ Assets/_Scripts/Data/DataManager.cs
@@ -1,10 +1,47 @@
-﻿using System.IO;
+﻿using _Scripts;
+using System.IO;
 using UnityEngine;
 
 namespace Assets._Scripts
 {
-    internal class DataManager:MonoBehaviour
+    internal class DataManager : MonoBehaviour
     {
+        [SerializeField] private GameData gameData;
+        private string filePath;
 
+        private void Awake( )
+        {
+            filePath = Path.Combine(Application.persistentDataPath,"gamedata.json");
+        }
+
+        public void SaveGameData( )
+        {
+            GameDataSerializable data = new GameDataSerializable(gameData);
+            string jsonData = JsonUtility.ToJson(data,true);
+            File.WriteAllText(filePath,jsonData);
+            Debug.Log("Game data saved to " + filePath);
+        }
+
+        public void LoadGameData( )
+        {
+            if (File.Exists(filePath))
+            {
+                string jsonData = File.ReadAllText(filePath);
+                GameDataSerializable data = JsonUtility.FromJson<GameDataSerializable>(jsonData);
+
+                gameData.HighScoreData = data.highScoreData;
+                gameData.CurrentScoreData = data.currentScoreData;
+                gameData.FinalScore = data.finalScore;
+                gameData.Speed = data.speed;
+                gameData.Difficulty = data.difficulty;
+
+                Debug.Log("Game data loaded from " + filePath);
+            }
+            else
+            {
+                Debug.LogWarning("No save file found at " + filePath);
+            }
+        }
     }
 }
+
